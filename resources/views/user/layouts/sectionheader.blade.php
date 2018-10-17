7bbec4caa4836076952bd23abae66d2b141e8c20
jolie6617 <30284100+jolie6617@users.noreply.github.com>
2018-10-17 17:10:09 +0800

    Website template first commit

---
+++ resources/views/user/layouts/sectionheader.blade.php
@@ -0,0 +1,8 @@
+<div id="aboutus">
+
+</div>
+
+<header class="section-header mt-4">
+    <h3>@yield('sectionHeader')</h3>
+    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+</header>
