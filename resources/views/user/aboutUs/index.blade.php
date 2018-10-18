4b4cf947fa63fb37dc199e36832c1a23ec50c8b6
Aloy <aloysiusalexander63@yahoo.com>
2018-10-18 11:05:25 +0800

    Add files via upload
    about us first commit

---
+++ resources/views/user/aboutUs/index.blade.php
@@ -0,0 +1,31 @@
+@extends('user.layouts.master')
+
+@section('title', 'Create Feedback')
+
+@section('sectionHeader', 'About Us')
+
+@section('sectionHeaderSentence', 'We are a group of committed and passionate team members to serve the best consistent quality products and
+            provide heartfelt personal service towards our valued guests customers. We believe in quality and service
+            contributed by people who care and committed to people. With premium quality ingredients, excellence of
+            preparation, we guarantee that our customers will be satisfied with each and every pizza when is delivered right
+            at their doorstep.')
+
+@section('content')
+
+    <div id="store">
+        <h2>Store Location</h2>
+        Bayan Lepas, Penang, <br>
+        Malaysia. <br>
+        Email: info@pizzaordering.my
+    </div>
+
+    <br>
+    <br>
+
+    <div id="hours">
+        <h2>Working Hours</h2>
+        Monday - Sunday <br>
+        10:00am - 10:00pm
+    </div>
+
+@endsection
