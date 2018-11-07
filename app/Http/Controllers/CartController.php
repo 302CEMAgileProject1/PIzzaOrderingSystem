0baf315693d8505a1dbc74f645b6e5e9ea5f0a37
Junaidi <jsharulzaman@yahoo.com>
2018-11-07 18:54:46 +0800

    cart controller update

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -26,5 +26,12 @@ class CartController extends Controller
         Cart::add($request->id, $request->name, $request->price, 1);
 
         return redirect(route('product.index'));
+    }    
+    
+    public function destroy($id)
+    {
+        Cart::remove($id);
+
+        return redirect(route('cart.index'));
     }
 }
